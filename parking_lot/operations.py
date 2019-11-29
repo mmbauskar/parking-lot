453a8ab7b37e11667c801c37ac23af91e62a51d1
Makarand Bauskar <mbauskar@gmail.com>
2019-11-29 20:20:45 +0530

    [wip] create parking slot and parking for the the car in parking slot

--- parking_lot/operations.py
+++ parking_lot/operations.py
@@ -3,7 +3,16 @@ from __init__ import parking_info
 
 def create_parking_lot(size, *args):
     ''' create the parking lot and slots '''
-    pass
+
+    if not all([size, size.isdigit(), int(size) > 0]):
+        print 'Invalid Input'
+
+    # check if the parking slot is already created
+    if parking_info.get('slot_size', 0):
+        print 'Parking lot is already created, Please exit first and then create a lot'
+
+    parking_info.update({ 'slot_size': int(size) })
+    print 'Created parking lot with lot {0} slots'.format(size)
 
 def leave(slot, *args):
     ''' remove car's from the parking lot '''
@@ -12,4 +21,56 @@ def leave(slot, *args):
 def park(registration_no, color, *args):
     ''' park the car to parking lot '''
 
-    pass
+    # validate the input
+
+    if not all([registration_no, color]):
+        print 'Invalid Registration No or Color'
+
+    slot = 0
+
+    # first check if there are any empty parking spots available to park
+    # else park car in the next available parking space
+
+    slot_size = parking_info.get('slot_size', [])
+    parked_cars = parking_info.get('parked_cars', [])
+    cars_by_color = parking_info.get('cars_by_color', {})
+    empty_parking = parking_info.get('empty_parking', [])
+    cars_by_registration_no = parking_info.get('cars_by_registration_no', {})
+
+    if len(parked_cars) >= slot_size:
+        print "Empty slot not available\n"
+        return
+    elif cars_by_registration_no.get(registration_no, None):
+        print "Invalid Registration No, car is already parked at slot {0}".format(
+            cars_by_registration_no.get(registration_no, None)
+        )
+        return
+
+    if len(empty_parking):
+        # empty_parking.sort()
+        slot = empty_parking.pop(0)
+    else:
+        slot = max(parked_cars) + 1 if parked_cars else 1
+        parked_cars.append(slot)
+        parked_cars.sort()
+
+    same_colored_car_slots = cars_by_color.get(color, [])
+    same_colored_car_slots.append(slot)
+
+    cars_by_color.update({
+        color.lower(): same_colored_car_slots
+    })
+
+    cars_by_registration_no.update({ registration_no: slot })
+
+    parking_info.update({
+        'parked_cars': parked_cars,
+        'empty_parking': empty_parking,
+        'cars_by_registration_no': cars_by_registration_no,
+        slot: {
+            'color': color,
+            'registration_no': registration_no
+        }
+    })
+
+    print "Allocated slot number: {0}\n".format(slot)
